2adf2ccbfe3b505570d38befa966fbabd537e98b
pruebaphp_trade <alejoyepes.18@gmail.com>
2020-05-18 15:12:55 -0500

    creacion de plantillas mvc

--- resources/views/pruebalogica.php
+++ resources/views/pruebalogica.php
@@ -1,5 +1,35 @@
-<?
-$z = '';
-$h = "&$z";
+<?php
+ function obtenermultipplpos($numerodado){
+    $multiplotres=0;
+    $multiplocinco=0;
+    for($i=0;$i<$numerodado;$i++){
+        if($i% 3==0){
+        $multiplotres += $i;
+
+        }  if($i% 5==0){
+            $multiplocinco += $i;
 
 
+            }
+
+            $multiplocinco += $multiplotres;
+            return  $multiplocinco;
+    }
+    function obtenermayus(){
+        $multiplotres="";
+        ucwords($multiplotres);
+
+        return  $multiplotres;
+        }
+
+
+
+
+
+
+}
+
+
+
+
+?>
